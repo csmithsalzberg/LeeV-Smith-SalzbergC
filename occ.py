062d65140a4d1cda2406171ad0f425bfc38df65f
vivienlee00 <vivien.lee.601@gmail.com>
2017-09-28 13:00:41 -0400

    updated from code reviews

--- occ.py
+++ occ.py
@@ -3,11 +3,15 @@
 #HW5 -- ...and Now Enjoy Its Contents
 #2017-09-26
 
-dict={}
-file = open('occupations.csv','r')
-ugly = file.read()
-lines = ugly.split("\n")
 import random
+
+
+def readin(x):
+    file = open(x,'r')
+    plaintext = file.read()
+    return plaintext
+
+lines = readin('occupations.csv').split("\n")
 
 #delete unnecessary lines from csv file
 del lines[0]
@@ -15,29 +19,33 @@ del lines[len(lines)-1]
 del lines[len(lines)-1]
 
 #adding each line to dictionary
+dict={}
 for line in lines:
     if line[0]=="\"":
         dict[line[0:(line.index("\"",1)+1)]] = line[(line.index("\"",1)+2):]
     else:
         dict[line.split(",")[0]]=line.split(",")[1]
 
-#jobs = []
-
-#for key in dict:
-#    jobs.append(key)
-#    jobs.append(dict[key])
 def randoc():
+    
     '''
     Prints the name of a random weighted occupation
+    
     Arg:
     none
+    
     Ret:
     str name of selected weighted occupation
+    
+    Algorithm:
+    each occupation from the csv has a certain range of numbers,
+    which is the sum of the previous ranges plus the occupation's percentage. 
     '''
-    magic =random.randint(0,1000)/10.0
+    
+    randomnum = random.randint(0,1000)/10.0
     counter = 0
     for key in dict:
-        if (counter + float(dict[key])) - magic > 0:
+        if (counter + float(dict[key])) - randomnum > 0:
             return key
         else:
             counter += float(dict[key])
